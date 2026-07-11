b93025338d6c035e8db3af7c0111aaae3b6a0a27
Shivam Kumar <shivam.sk2003@gmail.com>
2026-07-12 03:03:31 +0530

    Make themes adaptive across text and diagrams

--- site/modern.js
+++ site/modern.js
@@ -13,7 +13,7 @@
     if (existingCss) {
       var freshCss = document.createElement("link");
       freshCss.rel = "stylesheet";
-      freshCss.href = existingCss.href.split("?")[0] + "?v=42";
+      freshCss.href = existingCss.href.split("?")[0] + "?v=43";
       freshCss.setAttribute("data-cvam-modern-css", "true");
       document.head.appendChild(freshCss);
     }
